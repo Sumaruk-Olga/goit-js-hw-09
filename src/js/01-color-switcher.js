d410d461d76461b1cb5c8f630f8b3e6b7c7f76bd
Olga Sumaruk <olga_sumaruk@hotmail.com>
2022-05-30 16:27:52 +0300

    01-color-switcher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const refs = {
+    body: document.querySelector('body'),
+    start: document.querySelector('button[data-start]'),
+    stop: document.querySelector('button[data-stop]'),
+};
 
+
+refs.stop.disabled = true;
+
+let timerId;
+
+refs.start.addEventListener('click', onStartClick);
+refs.stop.addEventListener('click', onStopClick);
+
+
+
+function onStartClick(event) {
+    refs.stop.disabled = false;
+    refs.start.disabled = true;
+
+    timerId = setInterval(changeBodyBgc, 1000);
+}
+
+function onStopClick(event) {
+    refs.stop.disabled = true;
+    refs.start.disabled = false;
+    clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBodyBgc() {
+    refs.body.style.backgroundColor = getRandomHexColor();
+}
